e0932fb4bde567292981dc1165c9b190d5db353a
myeung30 <myeung30@my.centennialcollege.ca>
2023-11-03 10:32:36 -0400

    api added

--- src/lib/components/TuiCalendar.svelte
+++ src/lib/components/TuiCalendar.svelte
@@ -1,62 +1,122 @@
 <script>
-    import {onMount} from 'svelte';
+    import { onMount, onDestroy } from 'svelte';
     import 'tui-calendar/dist/tui-calendar.css';
     import Calendar from 'tui-calendar';
 
     let calendar;
     let viewMode = 'month';
     let isCurrentWeek = true;
-
-    onMount(() => {
-        calendar = new Calendar('#calendar', {
+    let currentDate = new Date();
+    let year = currentDate.getFullYear(); // Get current year
+    let month = currentDate.getMonth() + 1; // Get current month (0-11)
+    
+    calendar = new Calendar('#calendar', {
             defaultView: viewMode,
             taskView: false,
             template: {
                 monthDayname: day => `<span class="calendar-week-dayname">${day.label}</span>`,
             },
         });
-    });
 
-    function isViewingCurrentWeek() {
-        const currentDate = new Date();
-        const calendarDate = calendar.getDate();
+        async function fetchEventsByMonth(year, month) {
+    const response = await fetch(`http://localhost:3000/api/events/${year}/${month}`);
+    const data = await response.json();
+    return data;
+  }
 
-        return currentDate.getFullYear() === calendarDate.getFullYear() &&
-            currentDate.getMonth() === calendarDate.getMonth() &&
-            currentDate.getDate() === calendarDate.getDate();
+  onMount(async () => {
+    loadEvents(year, month);
+  });
+
+  onDestroy(() => {
+    calendar.destroy();
+  });
+
+  function loadEvents(year, month) {
+    fetchEventsByMonth(year, month)
+      .then(monthlyEvents => {
+        if (calendar) {
+          calendar.clear(); // Clear existing events
+          monthlyEvents.forEach(event => {
+            calendar.createSchedules([
+              {
+                id: event._id,
+                calendarId: '1',
+                title: event.Subject,
+                category: 'time',
+                dueDateClass: '',
+                start: event.FromDate,
+                end: event.ToDate,
+              }
+            ]);
+          });
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching events:', error);
+      });
+  }
+
+  function isViewingCurrentWeek() {
+    const currentDate = new Date();
+    const calendarDate = calendar.getDate();
+    return (
+      currentDate.getFullYear() === calendarDate.getFullYear() &&
+      currentDate.getMonth() === calendarDate.getMonth() &&
+      currentDate.getDate() === calendarDate.getDate()
+    );
+  }
+
+  function prevMonth() {
+    calendar.prev();
+    if(viewMode == 'month'){
+        if (month === 1) {
+            month = 12;
+            year -= 1;
+        } else {
+            month -= 1;
+        }
+    };
+    loadEvents(year, month);
+    isCurrentWeek = isViewingCurrentWeek();
+  }
+
+  function nextMonth() {
+    calendar.next();
+    if(viewMode == 'month'){
+        if (month === 12) {
+            month = 1;
+            year += 1;
+        } else {
+            month += 1;
+        }  
     }
+    loadEvents(year, month);
+    isCurrentWeek = isViewingCurrentWeek();
+  }
 
-    function prevWeek() {
-        calendar.prev();
-        isCurrentWeek = isViewingCurrentWeek();
-    }
+  function switchView() {
+    viewMode = viewMode === 'month' ? 'day' : 'month';
+    calendar.changeView(viewMode, true);
+  }
 
-    function nextWeek() {
-        calendar.next();
-        isCurrentWeek = isViewingCurrentWeek();
-    }
-
-    function switchView() {
-        viewMode = viewMode === 'month' ? 'day' : 'month';
-        calendar.changeView(viewMode, true);
-    }
-
-    function goToCurrentWeek() {
-        isCurrentWeek = true;
-        calendar.today();
-    }
+  function goToCurrentMonth() {
+    isCurrentWeek = true;
+    calendar.today();
+  }
 </script>
 
 <button on:click={switchView}>Switch View</button>
-<button on:click={prevWeek}>&lt;</button>
-<button on:click={nextWeek}>&gt;</button>
+<button on:click={prevMonth}>&lt;</button>
+<button>{month}, {year}</button>
+<button on:click={nextMonth}>&gt;</button>
 
 {#if !isCurrentWeek}
-    <button on:click={goToCurrentWeek}>Go to Current Week</button>
+  <button on:click={goToCurrentMonth}>Go to Current Month</button>
 {/if}
 
 <div id="calendar"></div>
 
 <style>
-    @import "../../styles/pages/calendar.scss";
+  @import "../../styles/pages/calendar.scss";
 </style>
